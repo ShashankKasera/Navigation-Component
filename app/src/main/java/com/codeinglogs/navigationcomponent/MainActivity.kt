7baa448cc81e24b0d87921311cfb6ef7d2782533
ShashankKasera <ShashankKasera98@gmail.com>
2021-12-12 02:02:52 +0530

    Setup ToolBar & ActionBar with Navigation C

--- app/src/main/java/com/codeinglogs/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/codeinglogs/navigationcomponent/MainActivity.kt
@@ -2,16 +2,30 @@ package com.codeinglogs.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.codeinglogs.navigationcomponent.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController=navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()||super.onSupportNavigateUp()
     }
 }
